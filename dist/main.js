917e5bc6fd1b1ab627de54314595824613c32155
charissa-project <charissameizzavardhani@gmail.com>
2026-05-28 14:50:45 +0700

    add swagger

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const http_exception_filter_1 = require("./common/filters/http-exception.filter");
 const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
 require("dotenv/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
         whitelist: true,
         transform: true
     }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('PawCare API')
+        .setDescription('API documentation for PawCare')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.useGlobalFilters(
-   new HttpExceptionFilter()
- )
- app.useGlobalPipes(
+  
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true
+  }));
 
- new ValidationPipe({
+  const config = new DocumentBuilder()
+    .setTitle('PawCare API')
+    .setDescription('API documentation for PawCare')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-   whitelist:true,
-
-   transform:true
-
- })
-
-)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
