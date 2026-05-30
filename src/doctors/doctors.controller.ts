2cfe354b7e856a0ff569616c596fe45860ace68a
charissa-project <charissameizzavardhani@gmail.com>
2026-05-30 16:37:40 +0700

    fix: add multer interceptor to parse form-data

--- src/doctors/doctors.controller.ts
+++ src/doctors/doctors.controller.ts
@@ -1,63 +1,35 @@
-import {
- Body,
- Controller,
- Get,
- Post,
- UseGuards
-} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 
-import { DoctorsService }
-from './doctors.service';
+import { DoctorsService } from './doctors.service';
+import { CreateDoctorDto } from './dto/create-doctor.dto';
+import { JwtGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from '../common/upload.config';
 
-import { CreateDoctorDto }
-from './dto/create-doctor.dto';
-
-import { JwtGuard }
-from '../auth/guards/jwt.guard';
-
-import { RolesGuard }
-from '../auth/guards/roles.guard';
-
-import { Roles }
-from '../auth/decorators/roles.decorator';
 
 @Controller('doctors')
-
 export class DoctorsController{
-
  constructor(
   private doctorService:
   DoctorsService
  ){}
 
  @Post()
-
- @UseGuards(
-  JwtGuard,
-  RolesGuard
- )
-
- @Roles('ADMIN')
-
- create(
- @Body()
- dto:CreateDoctorDto
- ){
-
-   return this
-   .doctorService
-   .create(dto)
-
- }
+@UseGuards(JwtGuard, RolesGuard)
+@Roles('ADMIN')
+@UseInterceptors(FileInterceptor('photo', multerConfig))
+create(@Body() dto: CreateDoctorDto) {
+  return this.doctorService.create(dto);
+}
 
  @Get()
-
  findAll(){
-
    return this
    .doctorService
    .findAll()
-
  }
 
 }
