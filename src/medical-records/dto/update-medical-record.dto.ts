caf84b3ce193b9a0edec9d835609856a541817ee
charissa-project <charissameizzavardhani@gmail.com>
2026-06-02 14:19:38 +0700

    fix update medical records api property

--- src/medical-records/dto/update-medical-record.dto.ts
+++ src/medical-records/dto/update-medical-record.dto.ts
@@ -1,14 +1,18 @@
 import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateMedicalRecordDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   diagnosis?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   treatment?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   notes?: string;
