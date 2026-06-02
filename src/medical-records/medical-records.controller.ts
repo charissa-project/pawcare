3e0956531d637cf4fd6e33535f6c198d2bb127c4
charissa-project <charissameizzavardhani@gmail.com>
2026-06-02 14:21:32 +0700

    fix role doctor

--- src/medical-records/medical-records.controller.ts
+++ src/medical-records/medical-records.controller.ts
@@ -76,7 +76,7 @@ findAllAdmin() {
   // hanya admin yang bisa hapus
   @Delete(':id')
   @UseGuards(RolesGuard)
-  @Roles(Role.ADMIN)
+  @Roles(Role.ADMIN, Role.DOCTOR)
   remove(
     @Param('id', ParseIntPipe) id: number,
     @GetUser('id') userId: number,
