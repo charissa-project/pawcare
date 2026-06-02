5f120fab231c8165b3553d44cbc0af73bc7efb3c
charissa-project <charissameizzavardhani@gmail.com>
2026-06-02 18:51:40 +0700

    fix update payments

---
+++ src/orders/dto/update-payment.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
+import { PaymentStatus } from '@prisma/client';
+
+export class UpdatePaymentStatusDto {
+  @ApiProperty({ enum: PaymentStatus, example: 'PAID' })
+  @IsEnum(PaymentStatus)
+  paymentStatus: PaymentStatus;
+}
